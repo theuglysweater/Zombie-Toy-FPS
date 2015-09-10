12eea27066f07aa47ebdf406b7977a8e1107b309
theuglysweater <jmarks@IT102.mr.preble.com>
2015-09-10 13:42:41 -0400

    1

---
+++ Zombie Shooter/Assets/Lock.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lock : MonoBehaviour {
+
+	bool Paused = false;
+
+	void Start ()
+	{
+		Screen.lockCursor = true;
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Pause ();
+		}
+	}
+
+	bool Pause ()
+	{
+		if (Paused = false) {
+			Screen.lockCursor = false;
+			Paused = true;
+			Debug.Log(Paused);
+		} else
+		{
+s			Screen.lockCursor = true;
+			Paused = false;
+			Debug.Log(Paused);
+		}
+		return Paused;
+	}
+}
